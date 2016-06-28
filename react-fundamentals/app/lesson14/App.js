a4a37d1b0008fee6665312974b8a3b0483df4473
Jose Medina <josemedina921@gmail.com>
2016-06-27 22:57:09 -0700

    lesson 14

---
+++ react-fundamentals/app/lesson14/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const PersonRow = (props) => {
+    return (
+        <tr>
+          <td>{props.data.id}</td><td>{props.data.name}</td>
+        </tr>
+    )
+};
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            data: [
+                {id: 1, name: "Jose Medina"},
+                {id: 2, name: "Samuel Medina"},
+                {id: 3, name: "Johnnie Medina"},
+                {id: 4, name: "Mercy Fernandez"}
+            ]
+        };
+    }
+    render() {
+        const rows = this.state.data.map( person => {
+            return <PersonRow key={person.id} data={person} />
+        });
+
+        return (
+            <table>
+              <tbody>
+                {rows}
+              </tbody>
+            </table>
+        )
+    }
+}
+
+export default App;
