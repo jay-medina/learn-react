033a9529c9671b466b2786edacf8ea9b0392c982
Jose Medina <josemedina921@gmail.com>
2016-07-04 20:44:57 -0700

    update

--- redux-fundamentals/app/app.js
+++ redux-fundamentals/app/app.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-class Demo extends React.Component {
-    render() {
-        return (
-            <div>
-                Hello Demo
-            </div>
-        );
-    }
+class Demo extends Component {
+  render() {
+    return (
+      <div>
+        Demo
+      </div>
+    );
+  }
 }
 
 export default Demo;
