5cf211853db15ad921d9256ba312af2522c89a52
Jose Medina <jomedina@microsoft.com>
2016-07-14 16:26:44 -0700

    Adding Thread Reply Interaction

--- threadExample/app/components/threadReply.tsx
+++ threadExample/app/components/threadReply.tsx
@@ -1,13 +1,48 @@
 import * as React from 'react';
 
-class ThreadReply extends React.Component<{}, {}> {
+export interface ThreadReplyState {
+  replyStarted: boolean
+}
+
+class ThreadReply extends React.Component<{}, ThreadReplyState> {
+  constructor() {
+    super();
+
+    this.state = {
+      replyStarted: false
+    };
+  }
+  onClick() {
+    this.setState({replyStarted: true});
+  }
+  renderInitialView() {
+    return (
+      <div onClick={this.onClick.bind(this)} className="thread__replyInput">
+        Write a Reply
+      </div>
+    );
+  }
+  renderAfterClickView() {
+    return (
+      <input type="text" className="thread__replyInput" autoFocus/> 
+    )
+  }
+  renderSubView() {
+    if(this.state.replyStarted) {
+      return this.renderAfterClickView();
+    }
+    
+    return this.renderInitialView();
+  }
   render() {
     return (
-      <div className="threadReply">
-        <input type="text" className="thread__replyInput" placeholder="Write a comment..." />
+      <div className="threadReply start">
+        {this.renderSubView()}
       </div>
     );
   }
 }
 
 export default ThreadReply;
+
+/* input type="text" className="thread__replyInput" placeholder="Write a reply" */
