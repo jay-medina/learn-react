1e710bcbe569f873b9b7db9ab7d90b6affb595f5
Jose Medina <josemedina921@gmail.com>
2016-07-19 23:25:21 -0700

    moving the isactive check to a function

--- redux-fundamentals/app/todo/views/Footer.tsx
+++ redux-fundamentals/app/todo/views/Footer.tsx
@@ -13,12 +13,16 @@ function Footer<FooterProps>(props) {
     return () => setVisibilityFilter(filter);
   }
 
+  function isActive(filter) {
+    return selected === filter;
+  }
+
   return (
     <p>
       <label className="filteritem">Show:</label> 
-      <Link active={selected === 'SHOW_ALL'} onLinkClick={onLinkClick("SHOW_ALL")}>All</Link>
-      <Link active={selected === 'SHOW_ACTIVE'} onLinkClick={onLinkClick("SHOW_ACTIVE")}>Active</Link>
-      <Link active={selected === 'SHOW_COMPLETED'} onLinkClick={onLinkClick("SHOW_COMPLETED")}>Completed</Link>
+      <Link active={isActive('SHOW_ALL')} onLinkClick={onLinkClick("SHOW_ALL")}>All</Link>
+      <Link active={isActive('SHOW_ACTIVE')} onLinkClick={onLinkClick("SHOW_ACTIVE")}>Active</Link>
+      <Link active={isActive('SHOW_COMPLETED')} onLinkClick={onLinkClick("SHOW_COMPLETED")}>Completed</Link>
     </p>
   )
 }
