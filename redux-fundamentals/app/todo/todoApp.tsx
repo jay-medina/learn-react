bba122e49c56b97d3d839311b7f03e10e442a364
Jose Medina <josemedina921@gmail.com>
2016-07-25 22:05:58 -0700

    Starting over

--- redux-fundamentals/app/todo/todoApp.tsx
+++ redux-fundamentals/app/todo/todoApp.tsx
@@ -1,33 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {createStore} from 'redux';
-import TodoAppReducer, {ITodoAppReducer} from './reducers/todoAppReducer.ts';
-import {loadState, saveState} from './reducers/localstorage.ts';
-import * as _ from 'lodash';
-
-import TodoAppView from './views/Todo.tsx';
 
 export function initialize() {
-  const persistedState = loadState();
 
-  const store = createStore<ITodoAppReducer>(TodoAppReducer, persistedState);
   function print() {
-    const state = store.getState();
 
     ReactDOM.render(
-      <TodoAppView dispatch={store.dispatch} todos={state.todos} filter={state.visibilityFilter} />, 
+      <div>I am sad i started from scratch</div>, 
       document.getElementById('todoApp')
     );
   }
-
-  store.subscribe(print);
-
-  store.subscribe(_.throttle(() => {
-    console.log('update');
-    saveState({
-      todos: store.getState().todos
-    });
-  }, 1000));
 
   print();
 }
