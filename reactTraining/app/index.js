146c27c0dada3abe07428193a4b9a27e652c247c
Jose Medina <josemedina921@gmail.com>
2017-01-15 00:56:00 -0800

    adding a line for webpack dev server

--- reactTraining/app/index.js
+++ reactTraining/app/index.js
@@ -4,7 +4,7 @@ var ReactDOM = require('react-dom');
 var HelloWorld = React.createClass({
   render: function() {
     return (
-      <div>Hello World</div>
+      <div>Hello World asdf</div>
     );
   }
 });
