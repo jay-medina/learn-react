79cccb6a2c34566ad3bb28315064a2a18d2a646a
Jose Medina <josemedina921@gmail.com>
2017-01-15 00:35:00 -0800

    adding webpack stuff

--- reactTraining/webpack.config.js
+++ reactTraining/webpack.config.js
@@ -1,5 +1,19 @@
 module.exports = {
-  entry: [
-    './app/index.js'
-  ]
+  entry: {
+    app: './app/index.js'
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+  
 };
