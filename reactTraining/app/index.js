223ef79345559f9b42af44b9515768021d0f349b
Jose Medina <josemedina921@gmail.com>
2017-01-15 23:54:45 -0800

    finishing avatar class

--- reactTraining/app/index.js
+++ reactTraining/app/index.js
@@ -1,12 +1,42 @@
+var USER_DATA = {
+  name: 'Jose Medina',
+  username: 'mrfunkycold',
+  image: 'https://avatars1.githubusercontent.com/u/2460039?v=3&u=e198fc7d42981a6f30d8bc16357e5e57508d5cb8&s=400'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-  render: function() {
-    return (
-      <div>Hello World asdf</div>
-    );
-  }
-});
+var ProfilePic = function(props) {
+  return (
+    <img src={props.imageUrl} />
+  )
+};
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+var ProfileLink = function(props) {
+  return (
+    <div>
+      <a target="_blank" href={`https://github.com/${props.username}`}>
+        {props.username}
+      </a>
+    </div>
+  )
+};
+
+var ProfileName = function(props) {
+  return (
+    <div>{props.name}</div>
+  )
+};
+
+var Avatar = React.createClass({
+  render() {
+    return <div>
+      <ProfilePic imageUrl={this.props.user.image} />
+      <ProfileName name={this.props.user.name} />
+      <ProfileLink username={this.props.user.username} />
+    </div>
+  }
+})
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
