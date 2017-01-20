0038f30dad7e3bcf43569e79ab5b56235d986ab3
Jose Medina <josemedina921@gmail.com>
2017-01-19 21:55:47 -0800

    creating prompt container and routes

--- reactTraining/app/config/routes.js
+++ reactTraining/app/config/routes.js
@@ -7,11 +7,14 @@ var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+var PromptContainer = require('../containers/PromptContainer');
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
+      <Route path='playerOne' component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' component={PromptContainer} />
     </Route>
   </Router>
 );
