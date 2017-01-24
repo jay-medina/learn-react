c4b64e2d48e7d56999690e60c85c2ce7d0806d3e
Jose Medina <josemedina921@gmail.com>
2017-01-23 22:19:10 -0800

    fetches player data then sets player info state

--- reactTraining/app/components/ConfirmBattle.js
+++ reactTraining/app/components/ConfirmBattle.js
@@ -1,11 +1,15 @@
 var React = require('react');
 
+function puke(object) {
+  return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
+
 module.exports = function ConfirmBattle(props) {
   if(props.isLoading) {
     return <p> LOADING!</p>
   }
   
   return (
-    <div>ConfirmBattle</div>
+    <div>ConfirmBattle: {puke(props)}</div>
   )
 };
