648061f18b0d465c9b0e34913977dfd97f7b2f60
Jose Medina <josemedina921@gmail.com>
2017-02-08 08:30:57 -0800

    updating the board

--- fb_tutorial/app/index.js
+++ fb_tutorial/app/index.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
+  }
+  getNextValue() {
+    return this.state.xIsNext? 'X': 'O';
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.getNextValue();
+    this.setState({squares: squares, xIsNext: !this.state.xIsNext});
+  }
   renderSquare(i) {
-    return <Square />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
   }
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.getNextValue()}`;
     return (
       <div>
         <div className="status">{status}</div>
