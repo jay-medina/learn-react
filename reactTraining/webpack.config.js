0c0d162f8b076aa9300f5b52ffae798be7a70a0f
Jose Medina <josemedina921@gmail.com>
2017-04-22 17:37:27 -0700

    remove everything

--- reactTraining/webpack.config.js
+++ reactTraining/webpack.config.js
@@ -1,28 +1,4 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './app/index.js'
-  },
-  output: {
-    path: __dirname + '/dist',
-    filename: 'index_bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: __dirname + '/app/index.html',
-      filename: 'index.html',
-      inject: 'body'
-    })
-  ]
-  
+
 };
