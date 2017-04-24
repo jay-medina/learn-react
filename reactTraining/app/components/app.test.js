9dab080347d5cb96bbf859e0b53d5a1d8313b491
Jose Medina <josemedina921@gmail.com>
2017-04-23 22:57:22 -0700

    Adding the popular menu bar

--- reactTraining/app/components/app.test.js
+++ reactTraining/app/components/app.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './app';
-import Badge from './badge';
+import Popular from './popular';
 
 describe('<App />', function () {
   let component;
