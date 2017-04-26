3cb360d5acc990aeac3ce60d655bbab86da985ef
Jose Medina <josemedina921@gmail.com>
2017-04-25 21:52:03 -0700

    fix test and update config

--- reactTraining/webpack.config.js
+++ reactTraining/webpack.config.js
@@ -1,11 +1,12 @@
+const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './app/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
   },
   devtool: 'inline-source-map',
   module: {
@@ -17,6 +18,17 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module) {
+          // this assumes your vendor imports exist in the node_modules directory
+          return module.context && module.context.indexOf('node_modules') !== -1;
+      }
+    }),
+    //CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+    new webpack.optimize.CommonsChunkPlugin({ 
+      name: 'manifest', //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
     })
   ]
 };
