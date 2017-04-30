d45328708bf260997257ba6cf7694b39a94d6068
Jose Medina <josemedina921@gmail.com>
2017-04-29 17:10:15 -0700

    add nav and other pages

---
+++ reactTraining/app/components/battle.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Battle extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        Battle
+      </div>
+    );
+  }
+}
+
+export default Battle;
