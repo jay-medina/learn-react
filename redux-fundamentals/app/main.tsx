0919a0e6017360e4b91709c0146c461d01082893
Jose Medina <josemedina921@gmail.com>
2017-06-04 21:29:03 -0700

    start over

--- redux-fundamentals/app/main.tsx
+++
@@ -1,5 +0,0 @@
-import * as CounterApp from './counter/counterApp.tsx';
-import * as TodoApp from './todo/todoApp.tsx';
-
-CounterApp.initialize();
-TodoApp.initialize();
--- redux-fundamentals/typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/lodash/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/react-dom/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
-/// <reference path="modules/node-uuid/index.d.ts" />
