81fbac32bc7fb0d62cc58e884d4cbdcd7960c01b
Jose Medina <josemedina921@gmail.com>
2017-06-06 21:50:16 -0700

    first redux app

--- redux-fundamentals/app/main.ts
+++ redux-fundamentals/app/main.ts
@@ -1,15 +1,18 @@
-
-
-// console.log(store.getState());
-
-// store.subscribe(() => {
-//   document.body.innerText = `${store.getState()}`;
-// })
-
-// document.addEventListener('click', () => {
-//   store.dispatch<Action>({ type: 'INCREMENT'});
-// })
 import {createStore} from 'redux';
-import {counter} from './reducer/Counter';
+import { Action as CounterAction, counter} from './reducer/Counter';
 
 const store = createStore(counter);
+
+console.log(store.getState());
+
+function render() {
+  document.body.innerText = `My count is ${store.getState()}`;
+}
+store.subscribe(render)
+
+document.addEventListener('click', () => {
+  store.dispatch<CounterAction>({ type: 'INCREMENT'});
+});
+
+render();
+
