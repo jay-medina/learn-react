520dfb1a67c0dc9d1111d1ac406dca4309e6eb4e
Jose Medina <josemedina921@gmail.com>
2017-06-20 23:50:41 -0700

    updating the counter test

--- redux-fundamentals/app/counter.test.tsx
+++ redux-fundamentals/app/counter.test.tsx
@@ -1,12 +1,41 @@
 jest.mock('redux');
 
-import { createStore } from 'redux';
-
 describe('counter', function() {
-  beforeEach(() => {
-    require('./counter');
-  });
-  it('creates a store', function() {
-    expect(createStore).toHaveBeenCalledWith(expect.any(Function));
-  });  
+  let subscribe: any;
+  let getState: any;
+  let createStore: any;
+
+  describe('when running the app', function () {
+    beforeEach(() => {
+      jest.resetModules();
+      createStore = require('redux').createStore;
+
+      document.body.innerHTML = `
+        <div id="container"></div>
+      `;
+      subscribe = jest.fn();
+      getState = jest.fn();
+      (createStore as jest.Mock<any>).mockReturnValue({
+        subscribe,
+        getState,
+      });
+      require('./counter');
+    });
+
+
+    it('creates a store', function() {
+      expect(createStore).toHaveBeenCalledWith(expect.any(Function));
+    });
+
+    it('subscribes to the render function', function() {
+      expect(subscribe).toHaveBeenCalled();
+    });
+
+    it('gets the current state', function () {
+      expect(getState).toHaveBeenCalled();
+    });
+
+  })
+
+
 });
