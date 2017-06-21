ad9e0557d984c2775332fa101553d7d604bc5a73
Jose Medina <josemedina921@gmail.com>
2017-06-20 22:48:27 -0700

    adding coverage for jest tests

--- redux-fundamentals/app/counter/Counter.test.tsx
+++ redux-fundamentals/app/counter/Counter.test.tsx
@@ -2,47 +2,43 @@ import * as React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
 import { Counter } from './Counter';
 
-interface TestContext {
-  wrapper: ShallowWrapper<any, any>;
-  onDecrement: jest.Mock<any>;
-  onIncrement: jest.Mock<any>;
-  meow: string;
-}
 
 describe('counter/Counter', function() {
-  let context = {} as TestContext;
+  let wrapper: ShallowWrapper<any, any>;
+  let onDecrement: jest.Mock<any>;
+  let onIncrement: jest.Mock<any>;
 
   beforeEach(function () {
-    context.onDecrement = jest.fn();
-    context.onIncrement = jest.fn();
-    context.wrapper = shallow(
+    onDecrement = jest.fn();
+    onIncrement = jest.fn();
+    wrapper = shallow(
       <Counter
         value={123}
-        onDecrement={context.onDecrement}
-        onIncrement={context.onIncrement}
+        onDecrement={onDecrement}
+        onIncrement={onIncrement}
       />
     );
   });
 
   test('renders the value', function () {
-    expect(context.wrapper.find('h1').text()).toBe('123');
+    expect(wrapper.find('h1').text()).toBe('123');
   });
 
   it('renders the increment/decrement buttons', function () {
-    expect(context.wrapper.find('button').length).toBe(2);
+    expect(wrapper.find('button').length).toBe(2);
   });
 
   describe('when increment is clicked', function () {
     it('invokes the increment callback', function () {
-      context.wrapper.find('.counter__increment-btn').simulate('click');
-      expect(context.onIncrement).toHaveBeenCalled();
+      wrapper.find('.counter__increment-btn').simulate('click');
+      expect(onIncrement).toHaveBeenCalled();
     });
   });
 
   describe('when decrement is clicked', function () {
     it('invokes the decrement callback', function () {
-      context.wrapper.find('.counter__decrement-btn').simulate('click');
-      expect(context.onDecrement).toHaveBeenCalled();
+      wrapper.find('.counter__decrement-btn').simulate('click');
+      expect(onDecrement).toHaveBeenCalled();
     });
   });
 });
