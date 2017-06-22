7f99c13ad876582958406863ff6816b162fbd80f
Jose Medina <josemedina921@gmail.com>
2017-06-21 23:20:32 -0700

    adding ts lint

--- redux-fundamentals/app/counter/reducer.test.ts
+++ redux-fundamentals/app/counter/reducer.test.ts
@@ -1,46 +1,65 @@
-import { counter } from './reducer';
+import { counter, addCounter } from './reducer';
 
-describe('Counter', function () {
+describe('counter/reducer', function () {
 
-  describe('initial state', function () {
-    it('should return zero', function () {
-      expect(counter(undefined, {} as any)).toBe(0);
+  describe('counter', function () {
+    describe('initial state', function () {
+      it('should return zero', function () {
+        expect(counter(undefined, {} as any)).toBe(0);
+      });
+    });
+
+    describe('increment', function () {
+      describe('when count is zero', function () {
+        it('should become 1', function () {
+          expect(counter(0, { type: 'INCREMENT' })).toBe(1);
+        });
+      });
+
+      describe('when count is 1', function () {
+        it('should become 2', function () {
+          expect(counter(1, { type: 'INCREMENT' })).toBe(2);
+        });
+      });
+    });
+
+    describe('decrement', function () {
+      describe('when count is 2', function () {
+        it('should become 1', function () {
+          expect(counter(2, { type: 'DECREMENT' })).toBe(1);
+        });
+      });
+
+      describe('when count is 1', function () {
+        it('should become 0', function () {
+          expect(counter(1, { type: 'DECREMENT' })).toBe(0);
+        });
+      });
+    });
+
+    describe('unknown action', function () {
+      describe('when count is 2', function () {
+        it('should remain 2', function () {
+          expect(counter(2, { type: 'UNKNOWN' as any })).toBe(2);
+        });
+      });
     });
   });
 
-  describe('increment', function () {
-    describe('when count is zero', function () {
-      it('should become 1', function() {
-        expect(counter(0, {type: 'INCREMENT'})).toBe(1);
-      });
+  describe('addCounter', function() {
+    let listBefore: Array<number>;
+
+    beforeEach(function() {
+      listBefore = [];
     });
 
-    describe('when count is 1', function () {
-      it('should become 2', function() {
-        expect(counter(1, {type: 'INCREMENT'})).toBe(2);
-      });
+    it('should add 0 to the counter list', function() {
+      expect(addCounter(listBefore)).toEqual([0]);
+    });
+
+    it('should be different objects', function() {
+      expect(addCounter(listBefore)).not.toBe([0]);
     });
   });
 
-  describe('decrement', function () {
-    describe('when count is 2', function () {
-      it('should become 1', function() {
-        expect(counter(2, {type: 'DECREMENT'})).toBe(1);
-      });
-    });
-
-    describe('when count is 1', function () {
-      it('should become 0', function() {
-        expect(counter(1, {type: 'DECREMENT'})).toBe(0);
-      });
-    });
-  });
-
-  describe('unknown action', function () {
-    describe('when count is 2', function () {
-      it('should remain 2', function() {
-        expect(counter(2, {type: 'UNKNOWN' as any})).toBe(2);
-      });
-    });
-  });
 });
--- redux-fundamentals/app/counter/reducer.ts
+++ redux-fundamentals/app/counter/reducer.ts
@@ -1,11 +1,19 @@
 export interface Action {
-  type: 'INCREMENT' | 'DECREMENT'
+  type: 'INCREMENT' | 'DECREMENT';
 }
 
 export function counter(state = 0, action: Action) {
-  switch( action.type ) {
+  switch ( action.type ) {
     case 'INCREMENT' : return state + 1;
     case 'DECREMENT' : return state - 1;
     default: return state;
   }
+}
+
+export function addCounter(list: Array<number>) {
+  return [...list, 0];
+}
+
+export function removeCounter(list: Array<number>, index: number) {
+  
 }
