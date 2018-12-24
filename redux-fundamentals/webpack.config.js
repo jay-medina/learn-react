c3b669b8a0e04116e3998a59fd3ea01158573a0b
Jose Medina <josemedina921@gmail.com>
2018-12-23 22:24:34 -0800

    finish reseting

--- redux-fundamentals/webpack.config.js
+++ redux-fundamentals/webpack.config.js
@@ -1,12 +1,19 @@
+// @ts-check
+
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+/**
+ * @type webpack.Configuration
+ */
 module.exports = {
   entry: './app/main.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app/index.js'
   },
+  mode: 'development',
   devServer: {
     inline: true,
     port: 8080
